8b1ec4e0d451b9825323be8c1f1afb733dd575fc
sipher <93847144+ilovebeingrich@users.noreply.github.com>
2023-06-15 04:10:41 -0500

    Updaed config and added emojis and stuff

--- heal/config.py
+++ heal/config.py
@@ -1,13 +1,22 @@
 from discord.ext.commands import (Flag, ColorConverter)
 
 class Authorzation:
-    token = ''
-    prefix = ''
+    token = 'Bot Token'
+    prefix = ','
     
     class database:
+        """Want to make you're on database go to https://supabase.com/ to create one"""
+        """
+        its easy to create one just to let you know for you skids..
+        """
         ...
 
 class Color:
-    normal = ''
-    warn = ''
-    approve = ''
+    """Jinx just uses white idk why this guy does its stupid :skull:"""
+    normal = 0xffffff
+    warn = 0xffffff
+    approve = 0xffffff
+    
+class Emoji:
+    approve = '<:approve:1118825630692282439>'
+    locked = '<:locked:1118825629362688092>'
